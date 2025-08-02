a410d818fadcffb2dcc85e057262e6204141fbeb
Saeed Gholami <saeed.ghollami@gmail.com>
2025-08-02 23:14:54 +0330

    Decide winner

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import random
 board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
 def set_to_board(x, y, hand):
     global board
-    if x < 3 and y < 3:
+    if (x < 3 and y < 3) and board[x][y] == " ":
         if hand == "user":
             board[x][y] = "O"
         else:
@@ -24,14 +24,44 @@ def draw_board():
     print('-'*13)
 
 def decide_winner():
-    pass
+    # Horizantal check
+    if board[0][0] ==  board[0][1] == board[0][2] == "O":
+        print("User win.")
+    elif board[1][0] ==  board[1][1] == board[1][2] == "O":
+        print("User win.")
+    elif board[2][0] ==  board[2][1] == board[2][2] == "O":
+        print("User win.")
+    # Vertical check
+    elif board[0][0] ==  board[1][0] == board[3][0] == "O":
+        print("User win.")
+    elif board[1][0] ==  board[1][1] == board[1][2] == "O":
+        print("User win.")
+    elif board[2][0] ==  board[2][1] == board[2][2] == "O":
+        print("User win.")
+    # computer
+    elif board[0][0] ==  board[0][1] == board[0][2] == "X":
+        print("Computer win.")
+    elif board[1][0] ==  board[1][1] == board[1][2] == "X":
+        print("Computer win.")
+    elif board[2][0] ==  board[2][1] == board[2][2] == "X":
+        print("Computer win.")
+    # Vertical check
+    elif board[0][0] ==  board[1][0] == board[3][0] == "X":
+        print("Computer win.")
+    elif board[1][0] ==  board[1][1] == board[1][2] == "X":
+        print("Computer win.")
+    elif board[2][0] ==  board[2][1] == board[2][2] == "X":
+        print("Computer win.")
+    else:
+        print("Draw")
+
+
+
 
 def computer_hand():
     x = random.randrange(3)
     y = random.randrange(3)
     set_to_board(x, y, "computer")
-
-
 
 def main():
     for _ in range(3):
@@ -41,6 +71,7 @@ def main():
         set_to_board(x, y, "user")
         computer_hand()
     draw_board()
+    decide_winner()
 
 
 if __name__ == "__main__":
